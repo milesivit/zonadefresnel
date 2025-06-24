2a4292de46f8c52e420533573d82f6c6cef18a14
milena <m.sivit@itecriocuarto.org.ar>
2025-06-24 19:40:33 -0300

    ultimoo

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,23 +1,31 @@
 function calcularFresnel() { 
-  const d = parseFloat(document.getElementById("distancia").value); //obtiene el valor que el usuario escribio como texto en el input
-  const f = parseFloat(document.getElementById("frecuencia").value); //obtiene el valor que el usuario escribio como texto en el input
-  if (isNaN(d) || isNaN(f) || d <= 0 || f <= 0) { //verifica si el valor no es un numero y q ambos valores sean mayores a 0
+  const d = parseFloat(document.getElementById("distancia").value);
+  const f = parseFloat(document.getElementById("frecuencia").value);
+
+  if (isNaN(d) || isNaN(f) || d <= 0 || f <= 0) {
     document.getElementById("resultado").textContent = "Por favor ingrese valores válidos.";
     return;
-  } //sino hace el calculo y muestra el resultado
-  const F1 = 8.656 * Math.sqrt(d / f); //funcion dada por el profe 8.656 * elevado(d/f)
-  document.getElementById("resultado").textContent = `Primer radio de Fresnel: ${F1.toFixed(2)} metros`; //tofixed(2) redondea a 2 decimales
+  }
 
-    //guardar en historial local
-    const nuevoRegistro = {
-      distancia: d,
-      frecuencia: f,
-      resultado: F1.toFixed(2)
-    };
-  
-    let historial = JSON.parse(localStorage.getItem("historialFresnel")) || [];
-    historial.push(nuevoRegistro);
-    localStorage.setItem("historialFresnel", JSON.stringify(historial));
-  
+  const F1 = 8.656 * Math.sqrt(d / f);
+
+  // Función para cortar sin redondear a 2 decimales
+  function cortarDosDecimales(num) {
+    return Math.floor(num * 100) / 100;
+  }
+
+  const F1Cortado = cortarDosDecimales(F1);
+
+  document.getElementById("resultado").textContent = `Primer radio de Fresnel: ${F1Cortado.toFixed(2)} metros`;
+
+  // Guardar en historial local
+  const nuevoRegistro = {
+    distancia: d,
+    frecuencia: f,
+    resultado: F1Cortado
+  };
+
+  let historial = JSON.parse(localStorage.getItem("historialFresnel")) || [];
+  historial.push(nuevoRegistro);
+  localStorage.setItem("historialFresnel", JSON.stringify(historial));
 }
-
